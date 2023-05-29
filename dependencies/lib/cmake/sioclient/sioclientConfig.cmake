35716ee6c976d9b8be005b8be731bc91738ef014
zhaoqun_zhong <zhongzhaoqun1991@outlook.com>
2023-05-29 20:10:34 +0800

    remove dependencies folder

--- dependencies/lib/cmake/sioclient/sioclientConfig.cmake
+++
@@ -1,8 +0,0 @@
-find_library(SIOCLIENT_LIBRARY sioclient sioclient_tls
-    PATHS "/home/sti/Documents/stella_vslam/dependencies/lib"
-)
-find_path(SIOCLIENT_INCLUDE_DIR sioclient/sio_client.h sioclient/sio_message.h sioclient/sio_socket.h
-    PATHS "/home/sti/Documents/stella_vslam/dependencies/include"
-)
-set(SIOCLIENT_LIBRARIES ${SIOCLIENT_LIBRARY})
-set(SIOCLIENT_INCLUDE_DIRS ${SIOCLIENT_INCLUDE_DIR})
